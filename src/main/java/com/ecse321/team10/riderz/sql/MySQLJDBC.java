0aa49fb402f6009b78f7b255740dbfa31f837b72
heqianw <heqianw@gmail.com>
2018-09-22 12:19:25 -0400

    Added basic logging capabilities, the rolling file logging is next

--- src/main/java/com/ecse321/team10/riderz/sql/MySQLJDBC.java
+++ src/main/java/com/ecse321/team10/riderz/sql/MySQLJDBC.java
@@ -1,5 +1,11 @@
 package com.ecse321.team10.riderz.sql;
 
-public class MySQLJDBC {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class MySQLJDBC {
+	private static final Logger logger = LogManager.getLogger(MySQLJDBC.class);
+	public static void main(String[] args) {
+		logger.info("Log this here");
+	}
 }
