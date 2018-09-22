e7b575ffdfcce975e9a9ccca6907c2f7a33e41c0
heqianw <heqianw@gmail.com>
2018-09-22 10:39:49 -0400

    First skeleton of maven spring

---
+++ src/main/java/com/ecse321/team10/riderz/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ecse321.team10.riderz;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RiderzApplication.class);
+	}
+
+}
