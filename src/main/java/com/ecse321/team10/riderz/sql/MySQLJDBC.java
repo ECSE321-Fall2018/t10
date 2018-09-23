669a665890308a0f8a4b03c06442df2ae991e0bb
heqianw <heqianw@gmail.com>
2018-09-22 20:46:58 -0400

    Added basic connection and close connection, need to add more commands

--- src/main/java/com/ecse321/team10/riderz/sql/MySQLJDBC.java
+++ src/main/java/com/ecse321/team10/riderz/sql/MySQLJDBC.java
@@ -1,9 +1,48 @@
 package com.ecse321.team10.riderz.sql;
 
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 public class MySQLJDBC {
 	private static final Logger logger = LogManager.getLogger(MySQLJDBC.class);
 	
+	private static Connection c;
+	
+	//=======================
+	// CONNECTIONS
+	//=======================
+	
+	public boolean connect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			//this holds dummy values for now
+	        c = DriverManager.getConnection("jdbc:mysql://localhost/feedback?user=sqluser&password=sqluserpw");
+	        logger.info("Connection to the database has been established.");
+	        Statement stmt = c.createStatement();
+	        return true;
+		}
+		catch(Exception e) {
+			logger.error(e.getClass().getName() + ": " + e.getMessage());
+			return false;
+		}
+	}
+	public boolean closeConnection() {
+		try {
+			if (c != null) {
+                c.close();
+                System.out.println("Connection to the database has been closed.");
+                return true;
+            }
+			return false;
+		}
+		catch(Exception e) {
+			logger.error(e.getClass().getName() + ": " + e.getMessage());
+			return false;
+		}
+	}
 }
