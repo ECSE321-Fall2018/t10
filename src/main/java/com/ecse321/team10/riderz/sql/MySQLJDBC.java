d2576230838c614cb2f3bf7bcbdb6356d2ae4ddc
heqianw <heqianw@gmail.com>
2018-09-22 21:43:33 -0400

    Added insertCar & enum class for carClass

--- src/main/java/com/ecse321/team10/riderz/sql/MySQLJDBC.java
+++ src/main/java/com/ecse321/team10/riderz/sql/MySQLJDBC.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.ecse321.team10.riderz.model.CarClass;
+
 
 public class MySQLJDBC {
 	private static final Logger logger = LogManager.getLogger(MySQLJDBC.class);
@@ -24,6 +26,16 @@ public class MySQLJDBC {
 	        c = DriverManager.getConnection("jdbc:mysql://localhost/feedback?user=sqluser&password=sqluserpw");
 	        logger.info("Connection to the database has been established.");
 	        Statement stmt = c.createStatement();
+	        
+	        String sqlCars = 	"CREATE TABLE IF NOT EXISTS cars " 		+
+	        					" (carId 		INT PRIMARY KEY		NOT NULL," 	+
+	        					"  seats 		INT 				NOT NULL," 	+
+	        					"  model 		VARCHAR(50) 		NOT NULL," 	+
+	        					"  year 		INT 				NOT NULL," 	+
+	        					"  gasRate 		INT 				NOT NULL," 	+
+	        					"  carClass 	VARCHAR(50) 		NOT NULL";
+	        
+	        stmt.executeUpdate(sqlCars);
 	        return true;
 		}
 		catch(Exception e) {
@@ -45,4 +57,24 @@ public class MySQLJDBC {
 			return false;
 		}
 	}
+	
+	//=======================
+	// CARS API
+	//=======================
+	
+	public boolean insertCar(int seats, String model, int year, int gasRate, CarClass carClass) {
+		String insertCar = String.format("INSERT INTO cars (seats, model, year, gasRate, carClass) "
+				+ "VALUES (%d, %s, %d, %d, %s)", seats, model, year, gasRate, carClass);
+		try {
+			if(c.createStatement().executeUpdate(insertCar) <= 0)
+				return false;
+			return true;
+		}
+		catch(Exception e) {
+			logger.error(e.getClass().getName() + ": " + e.getMessage());
+			return false;
+		}
+	}
+	
+	
 }
