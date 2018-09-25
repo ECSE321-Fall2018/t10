68d3c1d1a9b26ebacb8d4476a393a61c73b4fdbe
si-x-li <si.x.li@mail.mcgill.ca>
2018-09-25 00:26:22 -0400

    Adding Driver class and correcting User class

--- src/main/java/com/ecse321/team10/riderz/model/User.java
+++ src/main/java/com/ecse321/team10/riderz/model/User.java
@@ -1,37 +1,50 @@
 package com.ecse321.team10.riderz.model;
 
 public class User {
-	private String operator;
-	private double rating;
-	private int personsRated;
-	private int tripsCompleted;
+	private String username;
+	private String password;
+	private String email;
+	private String phone;
+	private String firstName;
+	private String lastName;
 
-	public User(String operator, double rating, int personsRated, int tripsCompleted) {
-		this.operator = operator;
-		this.rating = rating;
-		this.personsRated = personsRated;
-		this.tripsCompleted = tripsCompleted;
+	public User(String username, String password, String email,
+				String phone, String firstName, String lastName) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 
-	public String getOperator() {
-		return this.operator;
+	public String getUsername() {
+		return this.username;
 	}
 
-	public double getRating() {
-		return this.rating;
+	public String getPassword() {
+		return this.password;
 	}
 
-	public int getPersonsRated() {
-		return this.personsRated;
+	public String getEmail() {
+		return this.email;
 	}
 
-	public int getTripsCompleted() {
-		return this.tripsCompleted;
-	}	
+	public String getPhone() {
+		return this.phone;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
 
 	public String toString() {
-		return "[User] Operator: " + this.operator + " Rating: " + this.rating + 
-			   " Persons Rated: " + this.personsRated + " Trips Completed: " + 
-			   this.tripsCompleted;
+		return "[User] Username: " + this.username + " Password: " + this.password +
+			   " Email: " + this.email + " Phone: " + this.phone + " First Name: " +
+			   this.firstName + " Last Name: " + this.lastName;
 	}
 }
