2fd0ec55a89cc2748c86fca496ef3dbeed3a031c
heqianw <heqianw@gmail.com>
2018-09-29 22:25:12 -0400

    First test on sql

--- src/test/java/com/ecse321/team10/riderz/sql/TestMysSQLJDBC.java
+++ src/test/java/com/ecse321/team10/riderz/sql/TestMysSQLJDBC.java
@@ -3,11 +3,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.ecse321.team10.riderz.model.Car;
+import com.ecse321.team10.riderz.model.User;
+
 public class TestMysSQLJDBC {
+	MySQLJDBC sql = new MySQLJDBC();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInsertUser() {
+		sql.connect();
+		User testUser = new User("heqian", "password", "heqian@gmail.com", "514888888", "He Qian", "Wang");
+		assertEquals(true, sql.insertUser(testUser));
+		assertEquals(true, sql.deleteUser("heqian"));
+		sql.closeConnection();
 	}
-
 }
