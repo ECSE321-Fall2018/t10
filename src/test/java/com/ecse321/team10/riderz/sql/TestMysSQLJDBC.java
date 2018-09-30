9d487982418f63d47fdaf3ee59875f018e0bd332
heqianw <heqianw@gmail.com>
2018-09-29 20:41:13 -0400

    For issue #6, adding test case for JDBC and removing service files

---
+++ src/test/java/com/ecse321/team10/riderz/sql/TestMysSQLJDBC.java
@@ -0,0 +1,13 @@
+package com.ecse321.team10.riderz.sql;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMysSQLJDBC {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
