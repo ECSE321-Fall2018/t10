4de88fd11d6b3daaadc7534d0598650a25488e6f
MatSavoie <35614794+MatSavoie@users.noreply.github.com>
2018-10-04 17:01:36 -0400

    URLS and DB mapping for obtaining all accessible itinaries

--- src/main/java/com/ecse321/team10/riderz/controller/RouteController.java
+++ src/main/java/com/ecse321/team10/riderz/controller/RouteController.java
@@ -21,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecse321.team10.riderz.dto.ItineraryDto;
+import com.ecse321.team10.riderz.dto.UserDto;
 import com.ecse321.team10.riderz.model.Itinerary;
+import com.ecse321.team10.riderz.model.User;
 // Add 3 imports: itinerarydto, locationdto, reservationdto
 import com.ecse321.team10.riderz.sql.MySQLJDBC;
 
@@ -59,15 +61,15 @@ public class RouteController {
 	/**
 	 * Insert an itinerary
 	 * 
-	 * @param tripID
-	 * @param startingLongitude
-	 * @param startingLatitude
-	 * @param startingTime
-	 * @param endingLongitude
-	 * @param endingLatitude
-	 * @param endingTime
-	 * @param seatsLeft
-	 * @return
+	 * @param tripID             - An integer uniquely identifying a trip
+	 * @param startingLongitude  - A double representing the starting longitude
+	 * @param startingLatitude   - A double representing the starting latitude
+	 * @param startingTime       - A String representing the starting time
+	 * @param endingLongitude    - A double representing the ending longitude
+	 * @param endingLatitude     - A double representing the ending latitude
+	 * @param endingTime         - A String representing the ending time
+	 * @param seatsLeft          - An integer representing the number of seats left
+	 * @return itinerary         - The created itinerary
 	 */
 	@GetMapping("/insertItinerary/{tripID}/{startingLongitude}/{startingLatitude}/{startingTime}/{endingLongitude}/{endingLatitude}/{endingTime}/{seatsLeft}")
 	public ItineraryDto insertItinerary(@PathVariable("tripID") int tripID,
@@ -98,15 +100,15 @@ public class RouteController {
 	/**
 	 * Update an itinerary
 	 * 
-	 * @param tripID
-	 * @param startingLongitude
-	 * @param startingLatitude
-	 * @param startingTime
-	 * @param endingLongitude
-	 * @param endingLatitude
-	 * @param endingTime
-	 * @param seatsLeft
-	 * @return
+	 * @param tripID             - An integer uniquely identifying a trip
+	 * @param startingLongitude  - A double representing the starting longitude
+	 * @param startingLatitude   - A double representing the starting latitude
+	 * @param startingTime       - A String representing the starting time
+	 * @param endingLongitude    - A double representing the ending longitude
+	 * @param endingLatitude     - A double representing the ending latitude
+	 * @param endingTime         - A String representing the ending time
+	 * @param seatsLeft          - An integer representing the number of seats left
+	 * @return updatedItinerary  - the updated itinerary
 	 */
 	@GetMapping("/updateItinerary/{tripID}/{startingLongitude}/{startingLatitude}/{startingTime}/{endingLongitude}/{endingLatitude}/{endingTime}/{seatsLeft}")
 	public ItineraryDto updateItinerary(@PathVariable("tripID") int tripID,
@@ -138,8 +140,8 @@ public class RouteController {
 	/**
 	 * Delete an itinerary
 	 * 
-	 * @param tripID
-	 * @return
+	 * @param tripID     - An integer uniquely identifying a trip
+	 * @return message   - A string to indicate if the itinerary was deleted successfully or not.
 	 */
 	@GetMapping("/deleteItinerary/{tripID}")
 	public String deleteItinerary(@PathVariable("tripID") int tripID) {
@@ -148,10 +150,10 @@ public class RouteController {
 		sql.connect();
 		if (sql.deleteItinerary(deleteTripID)) {
 			sql.closeConnection();
-			return String.format("Itinerary %s was deleted", deleteTripID);
+			return String.format("Itinerary %s was deleted.", deleteTripID);
 		}
 		sql.closeConnection();
-		return "Itinerary does not exist";	
+		return "Itinerary does not exist.";	
 	}
 	
 	//For Testing purpose:
@@ -159,8 +161,8 @@ public class RouteController {
 	/**
 	 * Obtain the Itinerary information base on the tripID
 	 * 
-	 * @param tripID
-	 * @return
+	 * @param tripID     - An integer uniquely identifying a trip
+	 * @return itinerary - The Itinerary object if found, null otherwise.
 	 */
 	@GetMapping("/getItineraryByTripID/{tripID}")
 	public ItineraryDto getItineraryByTripID(@PathVariable("tripID") int tripID) {
@@ -176,18 +178,56 @@ public class RouteController {
 		return null;
 	}
 	
+	
+	//For Testing purpose:
+	// *** Delete old stuff in DB ***
+	//localhost:8088/insertItinerary/35/45.419980/-73.883442/2019-01-01 02:00:00.000/45.456180/-73.862320/2019-01-01 02:30:00.000/2
+	//localhost:8088/getItineraryNearDestination/45.456181/-73.862321/0.500000/2019-01-01 03:30:00.000
+	//localhost:8088/getItineraryNearDestination/45.456181/-73.862321/0.160000/2050-02-02 02:30:00.000
+	//localhost:8088/getItineraryNearDestination/10.456181/-10.862321/500000000.160000/2019-01-02 03:30:00.000
+	
+	//localhost:8088/insertItinerary/36/15.33534/12.44412/2019-01-01 01:00:00.000/45.456580/-73.869320/2019-01-01 01:30:00.000/3
+	//localhost:8088/getItineraryNearDestination/45.456880/-73.869920/1000.00000/2019-01-01 04:30:00.000
+	/**
+	 * Obtains all the Itineraries fitting search criteria based on a spherical distance
+	 * algorithm. Low search radius is recommended for accurate results.
+	 * 
+	 * @param endingLongitude	-	A double representing destination longitude.
+	 * @param endingLatitude	-	A double representing destination latitude.
+	 * @param maximumDistance	-	A double representing maximum search radius in meters. 
+	 * @param arrivalTime       -   A String representing preferred arrival time
+	 * @return itineraryList    -   A List of Itinerary representing the itineraries found base on the criteria. Return Null if none was found.
+	 */
+	@GetMapping("/getItineraryNearDestination/{endingLongitude}/{endingLatitude}/{maximumDistance}/{arrivalTime}")
+	public List<ItineraryDto> getItineraryNearDestination(@PathVariable("endingLongitude") double endingLongitude,
+															@PathVariable("endingLatitude") double endingLatitude,
+															@PathVariable("maximumDistance") double maximumDistance,
+															@PathVariable("arrivalTime") String arrivalTime){
+		
+		Timestamp arrivalTimeStamp = stringtoTimeStamp(arrivalTime);
+		
+		sql.connect();		
+		List<ItineraryDto> itineraryList = new ArrayList<ItineraryDto>();
+		for(Itinerary itinerary : sql.getItineraryNearDestination(endingLongitude, endingLatitude, maximumDistance, arrivalTimeStamp))
+			itineraryList.add(intineraryConvertToDto(itinerary));
+		sql.closeConnection();
+		return itineraryList;
+	}
+	
+	
+	
 	/**
 	 * Helper Method: convert a string to a timeStamp
 	 * 
-	 * @param timeString
-	 * @return
+	 * @param timeString - A time represented in a string
+	 * @return timeStamp - A time represented by a timeStamp
 	 */
 	private Timestamp stringtoTimeStamp (String timeString) {
 		try {
 		    SimpleDateFormat dateLayout = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
 		    Date date = dateLayout.parse(timeString);
-		    Timestamp timestamp = new java.sql.Timestamp(date.getTime());
-		    return timestamp;
+		    Timestamp timeStamp = new java.sql.Timestamp(date.getTime());
+		    return timeStamp;
 		} catch(Exception e) {
 			return null;
 		}
