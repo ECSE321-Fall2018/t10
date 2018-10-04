1bdc770081ad286279580bc85275ddd20d19e5ac
Tyrone Wong <tyronewong96@gmail.com>
2018-10-04 11:29:39 -0400

    Backend: Added add, get, getAll and delete endpoints in User controller
    - set the request mapping for users as "/users"
    - Requests endpoints added: addUser, getUser, getAllusers and deleteUser
    
    To-Do: Add logging and catch invalid input

--- src/main/java/com/ecse321/team10/riderz/controller/UsersController.java
+++ src/main/java/com/ecse321/team10/riderz/controller/UsersController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,7 @@ import com.ecse321.team10.riderz.model.User;
 import com.ecse321.team10.riderz.sql.MySQLJDBC;
 
 @RestController
+@RequestMapping("users")
 public class UsersController {
 	
 	@Autowired
@@ -32,16 +35,28 @@ public class UsersController {
 	
 	private static final Logger logger = LogManager.getLogger(RiderzController.class);
 	
-	@PostMapping("/addUser/{username}")
-	public UserDto addUser(	@PathVariable("username") String username,
+	@PostMapping("addUser")
+	public UserDto addUser(	@RequestParam String username,
 							@RequestParam String password,
 							@RequestParam String email,
 							@RequestParam String phone,
 							@RequestParam String firstName,
 							@RequestParam String lastName) {
 		User user = new User(username, password, email, phone, firstName, lastName);
-		sql.connect();
-		if(sql.insertUser(user)) {
+		if(sql.connect() && sql.insertUser(user)) {
+			sql.closeConnection();
+			return convertToDto(user);
+		}else{
+			sql.closeConnection();
+			return null;
+		}
+
+	}
+	
+	@GetMapping("getUser")
+	public UserDto getUser(	@RequestParam("username") String username) {
+		if(sql.connect()){
+			User user = sql.getUserByUsername(username);
 			sql.closeConnection();
 			return convertToDto(user);
 		}
@@ -49,21 +64,30 @@ public class UsersController {
 		return null;
 	}
 	
-	@GetMapping("/getUser/{username}")
-	public UserDto getUser(	@PathVariable("username") String username) {
-		sql.connect();
-		User user = sql.getUserByUsername(username);
-		sql.closeConnection();
-		return convertToDto(user);
-	}
-	
-	@GetMapping("/getAllUsers")
+	@GetMapping("getAllUsers")
 	public List<UserDto> getAllUser() {
-		sql.connect();
-		List<UserDto> users = new ArrayList<UserDto>();
-		for(User user : sql.getAllUsers())
-			users.add(convertToDto(user));
-		sql.closeConnection();
-		return users;
+		if(sql.connect()){
+			List<UserDto> users = new ArrayList<UserDto>();
+			for(User user : sql.getAllUsers()){
+				users.add(convertToDto(user));
+			}
+			sql.closeConnection();
+			return users;
+		}
+		else{
+			sql.closeConnection();
+			return null;
+		}
 	}
+
+	//Note: Will need a verification to allow this to happen
+	//otherwise anyone can just delete user via url
+	@DeleteMapping("deleteUser")
+	public void deleteUser(@RequestParam("username") String username){
+		if(sql.connect()){
+			sql.deleteUser(username);
+			sql.closeConnection();
+		}
+	}
+
 }
