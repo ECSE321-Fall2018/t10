449f8e281d523e1cb5507e8472cdd374ee396f43
MatSavoie <35614794+MatSavoie@users.noreply.github.com>
2018-10-05 18:04:42 -0400

    Attempt to fix Travis CI - Error mostly due to time zone issues (changed time in Itinerary)

--- src/test/java/com/ecse321/team10/riderz/controller/TestRouteController.java
+++ src/test/java/com/ecse321/team10/riderz/controller/TestRouteController.java
@@ -55,13 +55,14 @@ public class TestRouteController {
 	public void tearDown() {
 		sql.connect();
 		//sql.deleteItinerary(tripID);
-		sql.deleteItinerary(tripID2);
+		//sql.deleteItinerary(tripID2);
 		sql.deleteTrip(tripID, "unitTest-Sav");
 		sql.deleteTrip(tripID2, "unitTest-Sav");
 		sql.deleteUser("unitTest-Sav");
 		sql.closeConnection();
 	}
 	
+	/*
 	@Test
 	public void testItinerary () throws Exception {
 		
@@ -94,8 +95,48 @@ public class TestRouteController {
 		// Testing delete Itinerary by tripID
 		this.mockMvc.perform(get("/deleteItinerary/" + tripID)).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(containsString("Itinerary " +tripID +" was deleted.")));		
+		
+		this.mockMvc.perform(get("/deleteItinerary/" + tripID2)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Itinerary " +tripID2 +" was deleted.")));
 	}
+	*/
 	
+	@Test
+	public void testItinerary () throws Exception {
+		
+		// Testing insert Itinerary
+		// tripID
+		this.mockMvc.perform(get("/insertItinerary/" + tripID + "/22.2222/-33.33333/2050-01-02 02:00:00.000/12.232323/-52.525252/2051-01-01 02:30:00.000/1")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":22.2222,\"startingLatitude\":-33.33333,\"startingTime\":\"2050-01-02T02:00:00.000+0000\",\"endingLongitude\":12.232323,\"endingLatitude\":-52.525252,\"endingTime\":\"2051-01-01T02:30:00.000+0000\",\"seatsLeft\":1}")));
+	
+		// Testing insert Itinerary
+		// tripID2
+		this.mockMvc.perform(get("/insertItinerary/" + tripID2 + "/15.33534/12.44412/2019-01-02 02:00:00.000/45.45658/-73.86932/2019-01-02 02:30:00.000/1")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("{\"tripID\":"+ tripID2 +",\"startingLongitude\":15.33534,\"startingLatitude\":12.44412,\"startingTime\":\"2019-01-02T02:00:00.000+0000\",\"endingLongitude\":45.45658,\"endingLatitude\":-73.86932,\"endingTime\":\"2019-01-02T02:30:00.000+0000\",\"seatsLeft\":1}")));
+		
+		// Testing update Itinerary
+		this.mockMvc.perform(get("/updateItinerary/" + tripID + "/45.41998/-73.883442/2019-01-01 02:00:00.000/45.45618/-73.86232/2019-01-01 02:30:00.000/3")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":45.41998,\"startingLatitude\":-73.883442,\"startingTime\":\"2019-01-01T02:00:00.000+0000\",\"endingLongitude\":45.45618,\"endingLatitude\":-73.86232,\"endingTime\":\"2019-01-01T02:30:00.000+0000\",\"seatsLeft\":3}")));
+	
+		// Testing get Itinerary by tripID
+		this.mockMvc.perform(get("/getItineraryByTripID/" + tripID)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":45.41998,\"startingLatitude\":-73.883442,\"startingTime\":\"2019-01-01T02:00:00.000+0000\",\"endingLongitude\":45.45618,\"endingLatitude\":-73.86232,\"endingTime\":\"2019-01-01T02:30:00.000+0000\",\"seatsLeft\":3}")));
+	
+		// Testing get Itinerary near destinations (1 Itinerary)
+		this.mockMvc.perform(get("/getItineraryNearDestination/45.45688/-73.86992/1000.00000/2019-01-01 04:30:00.000")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":45.41998,\"startingLatitude\":-73.883442,\"startingTime\":\"2019-01-01T02:00:00.000+0000\",\"endingLongitude\":45.45618,\"endingLatitude\":-73.86232,\"endingTime\":\"2019-01-01T02:30:00.000+0000\",\"seatsLeft\":3}")));
+		
+		// Testing get Itineraries near destination (2 Itinerary)
+		this.mockMvc.perform(get("/getItineraryNearDestination/45.45688/-73.86992/1000000.00000/2019-01-02 04:30:00.000")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":45.41998,\"startingLatitude\":-73.883442,\"startingTime\":\"2019-01-01T02:00:00.000+0000\",\"endingLongitude\":45.45618,\"endingLatitude\":-73.86232,\"endingTime\":\"2019-01-01T02:30:00.000+0000\",\"seatsLeft\":3},{\"tripID\":"+ tripID2 +",\"startingLongitude\":15.33534,\"startingLatitude\":12.44412,\"startingTime\":\"2019-01-02T02:00:00.000+0000\",\"endingLongitude\":45.45658,\"endingLatitude\":-73.86932,\"endingTime\":\"2019-01-02T02:30:00.000+0000\",\"seatsLeft\":1}")));
+		
+		// Testing delete Itinerary by tripID
+		this.mockMvc.perform(get("/deleteItinerary/" + tripID)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Itinerary " +tripID +" was deleted.")));		
+		
+		this.mockMvc.perform(get("/deleteItinerary/" + tripID2)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Itinerary " +tripID2 +" was deleted.")));
+	}
 	
 	
 	
@@ -110,7 +151,7 @@ public class TestRouteController {
 	 * @param timeString - A time represented in a string
 	 * @return timeStamp - A time represented by a timeStamp
 	 */
-	
+	/*
 	private Timestamp stringtoTimeStamp (String timeString) {
 		try {
 		    SimpleDateFormat dateLayout = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
@@ -121,6 +162,6 @@ public class TestRouteController {
 			return null;
 		}
 	}
-	
+	*/
 	
 }
