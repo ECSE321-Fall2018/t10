7ed062c6012d12ae3f82b76a678b75cb57232faa
MatSavoie <35614794+MatSavoie@users.noreply.github.com>
2018-10-05 18:29:01 -0400

    Fixed time zone issues related to Travis CI build, finalized testing Itinerary, and refactored code.

--- src/test/java/com/ecse321/team10/riderz/controller/TestRouteController.java
+++ src/test/java/com/ecse321/team10/riderz/controller/TestRouteController.java
@@ -54,14 +54,17 @@ public class TestRouteController {
 	@After
 	public void tearDown() {
 		sql.connect();
-		//sql.deleteItinerary(tripID);
-		//sql.deleteItinerary(tripID2);
 		sql.deleteTrip(tripID, "unitTest-Sav");
 		sql.deleteTrip(tripID2, "unitTest-Sav");
 		sql.deleteUser("unitTest-Sav");
 		sql.closeConnection();
 	}
 	
+	/**
+	 * IMPORTANT NOTE: 
+	 * *** FOR LOCAL TESTING ONLY ***
+	 * The code below is commented. The reason being that when testing locally, the URLS return Timestamp that have a 5 hours difference. Time zone issue is at the source of this problem (only occurs locally).
+	 */
 	/*
 	@Test
 	public void testItinerary () throws Exception {
@@ -92,6 +95,14 @@ public class TestRouteController {
 		this.mockMvc.perform(get("/getItineraryNearDestination/45.45688/-73.86992/1000000.00000/2019-01-02 04:30:00.000")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":45.41998,\"startingLatitude\":-73.883442,\"startingTime\":\"2019-01-01T07:00:00.000+0000\",\"endingLongitude\":45.45618,\"endingLatitude\":-73.86232,\"endingTime\":\"2019-01-01T07:30:00.000+0000\",\"seatsLeft\":3},{\"tripID\":"+ tripID2 +",\"startingLongitude\":15.33534,\"startingLatitude\":12.44412,\"startingTime\":\"2019-01-02T07:00:00.000+0000\",\"endingLongitude\":45.45658,\"endingLatitude\":-73.86932,\"endingTime\":\"2019-01-02T07:30:00.000+0000\",\"seatsLeft\":1}")));
 		
+		// Testing increment seats left by tripID
+		this.mockMvc.perform(get("/incrementSeatsLeft/" + tripID)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("The number of seats left in the itinerary " + tripID +" was incremented.")));
+		
+		// Testing decrement seats left by tripID
+		this.mockMvc.perform(get("/decrementSeatsLeft/" + tripID)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("The number of seats left in the itinerary "+ tripID +" was decremented.")));
+		
 		// Testing delete Itinerary by tripID
 		this.mockMvc.perform(get("/deleteItinerary/" + tripID)).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(containsString("Itinerary " +tripID +" was deleted.")));		
@@ -101,6 +112,12 @@ public class TestRouteController {
 	}
 	*/
 	
+	/**
+	 * 
+	 * IMPORTANT NOTE:
+	 * *** Testing for Travis CI ***
+	 * 
+	 */
 	@Test
 	public void testItinerary () throws Exception {
 		
@@ -108,8 +125,7 @@ public class TestRouteController {
 		// tripID
 		this.mockMvc.perform(get("/insertItinerary/" + tripID + "/22.2222/-33.33333/2050-01-02 02:00:00.000/12.232323/-52.525252/2051-01-01 02:30:00.000/1")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":22.2222,\"startingLatitude\":-33.33333,\"startingTime\":\"2050-01-02T02:00:00.000+0000\",\"endingLongitude\":12.232323,\"endingLatitude\":-52.525252,\"endingTime\":\"2051-01-01T02:30:00.000+0000\",\"seatsLeft\":1}")));
-	
-		// Testing insert Itinerary
+		
 		// tripID2
 		this.mockMvc.perform(get("/insertItinerary/" + tripID2 + "/15.33534/12.44412/2019-01-02 02:00:00.000/45.45658/-73.86932/2019-01-02 02:30:00.000/1")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(containsString("{\"tripID\":"+ tripID2 +",\"startingLongitude\":15.33534,\"startingLatitude\":12.44412,\"startingTime\":\"2019-01-02T02:00:00.000+0000\",\"endingLongitude\":45.45658,\"endingLatitude\":-73.86932,\"endingTime\":\"2019-01-02T02:30:00.000+0000\",\"seatsLeft\":1}")));
@@ -129,6 +145,14 @@ public class TestRouteController {
 		// Testing get Itineraries near destination (2 Itinerary)
 		this.mockMvc.perform(get("/getItineraryNearDestination/45.45688/-73.86992/1000000.00000/2019-01-02 04:30:00.000")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(containsString("{\"tripID\":"+ tripID +",\"startingLongitude\":45.41998,\"startingLatitude\":-73.883442,\"startingTime\":\"2019-01-01T02:00:00.000+0000\",\"endingLongitude\":45.45618,\"endingLatitude\":-73.86232,\"endingTime\":\"2019-01-01T02:30:00.000+0000\",\"seatsLeft\":3},{\"tripID\":"+ tripID2 +",\"startingLongitude\":15.33534,\"startingLatitude\":12.44412,\"startingTime\":\"2019-01-02T02:00:00.000+0000\",\"endingLongitude\":45.45658,\"endingLatitude\":-73.86932,\"endingTime\":\"2019-01-02T02:30:00.000+0000\",\"seatsLeft\":1}")));
+		
+		// Testing increment seats left by tripID
+		this.mockMvc.perform(get("/incrementSeatsLeft/" + tripID)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("The number of seats left in the itinerary " + tripID +" was incremented.")));
+		
+		// Testing decrement seats left by tripID
+		this.mockMvc.perform(get("/decrementSeatsLeft/" + tripID)).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("The number of seats left in the itinerary "+ tripID +" was decremented.")));
 		
 		// Testing delete Itinerary by tripID
 		this.mockMvc.perform(get("/deleteItinerary/" + tripID)).andDo(print()).andExpect(status().isOk())
