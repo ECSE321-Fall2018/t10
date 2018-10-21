f513ece9cfcde7dffa6019c363b43f2e91b5f001
si-x-li <si.x.li@mail.mcgill.ca>
2018-10-21 17:40:31 -0400

    Adding Android skeleton and modifying .travis.yml for Android

---
+++ android_passengers/app/src/main/java/riderz/team10/ecse321/com/riderzpassengers/RiderzPassengers.java
@@ -0,0 +1,13 @@
+package riderz.team10.ecse321.com.riderzpassengers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiderzPassengers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riderz_passengers);
+    }
+}
