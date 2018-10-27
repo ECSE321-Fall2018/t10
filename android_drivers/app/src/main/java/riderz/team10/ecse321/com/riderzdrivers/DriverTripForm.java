1c6fa8aa28cd90dfac3ba09f7c6439db19484a71
He Qian Wang <heqianw@gmail.com>
2018-10-27 15:30:17 -0400

    done basics

--- android_drivers/app/src/main/java/riderz/team10/ecse321/com/riderzdrivers/DriverTripForm.java
+++ android_drivers/app/src/main/java/riderz/team10/ecse321/com/riderzdrivers/DriverTripForm.java
@@ -1,5 +1,6 @@
 package riderz.team10.ecse321.com.riderzdrivers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,7 @@ import android.view.View;
 
 public class DriverTripForm extends AppCompatActivity {
 
+    //to do link the main page to this page
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +28,13 @@ public class DriverTripForm extends AppCompatActivity {
         });
     }
 
+    //intent, pair of values
+    //intent.put extra
+    //start address and end address
+    //keys are "startAddress" and "endAddress"
+    //values are what we get from form
     public void newTrip(View view) {
+        Intent intent = new Intent();
+        intent.putExtra();
     }
 }
