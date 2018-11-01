3b879e692af5bc543c24e7d251c03d817e2806c9
heqianw <heqianw@gmail.com>
2018-10-31 23:33:15 -0400

    Added drivers profile

---
+++ android_drivers/app/src/main/java/riderz/team10/ecse321/com/riderzdrivers/DriverProfile.java
@@ -0,0 +1,14 @@
+package riderz.team10.ecse321.com.riderzdrivers;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DriverProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_profile);
+    }
+
+}
---
+++ android_drivers/app/src/main/java/riderz/team10/ecse321/com/riderzdrivers/http/DriverProfile.java
@@ -0,0 +1,16 @@
+package riderz.team10.ecse321.com.riderzdrivers.http;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import riderz.team10.ecse321.com.riderzdrivers.R;
+
+public class DriverProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_profile);
+    }
+
+}
