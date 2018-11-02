71b4c78a4f8d9c72fcc6f0ceff13a10fb844b5ee
Mei Qi Tang <mei.q.tang@mail.mcgill.ca>
2018-11-02 05:03:59 -0400

    Added the distance and calculator file Added the time converter file Added price display and formatted time

---
+++ android_passengers/app/src/main/java/riderz/team10/ecse321/com/riderzpassengers/SQLCompliance.java
@@ -0,0 +1,16 @@
+package riderz.team10.ecse321.com.riderzpassengers;
+
+public class SQLCompliance {
+    /**
+     * Converts a timestamp in String into a SQL compliant timestamp.
+     * @param timestamp A String representing a timestamp.
+     * @return A formatted timestamp.
+     */
+    public static String convertToSQLTimestamp(String timestamp) {
+        timestamp = timestamp.replace("T", " ");
+        timestamp = timestamp.replace("+", "0");
+        int index = timestamp.indexOf(".");
+        timestamp = timestamp.substring(0, index);
+        return timestamp;
+    }
+}
