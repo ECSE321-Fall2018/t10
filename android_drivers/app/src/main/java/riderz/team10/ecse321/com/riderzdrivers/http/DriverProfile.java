373e012c6e2005ab44a6025923c7ae89aae6c53f
He Qian Wang <heqianw@gmail.com>
2018-11-01 20:22:16 -0400

    Added another http call for car

--- android_drivers/app/src/main/java/riderz/team10/ecse321/com/riderzdrivers/http/DriverProfile.java
+++ android_drivers/app/src/main/java/riderz/team10/ecse321/com/riderzdrivers/http/DriverProfile.java
@@ -3,6 +3,7 @@ package riderz.team10.ecse321.com.riderzdrivers.http;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -10,6 +11,7 @@ import com.loopj.android.http.SyncHttpClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 import riderz.team10.ecse321.com.riderzdrivers.R;
@@ -19,11 +21,12 @@ import riderz.team10.ecse321.com.riderzdrivers.constants.URL;
 
 public class DriverProfile extends Activity implements HttpRequestClient {
 
-    // Provides access
+    // Provides access in the url
     private String username;
 
-    // Current trips
-    private JSONArray json;
+    // Information concerning the driver
+    private JSONObject jsonDriver;
+    private JSONObject jsonCar;
 
 
     @Override
@@ -31,6 +34,32 @@ public class DriverProfile extends Activity implements HttpRequestClient {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_profile);
         username = getIntent().getStringExtra("username");
+        syncHttpRequest();
+        syncHttpRequestCar();
+        TextView driverName = (TextView)findViewById(R.id.driverNameView);
+        TextView driverRating = (TextView)findViewById(R.id.driverRatingView);
+        TextView driverCarMake = (TextView)findViewById(R.id.driverCarMakeView);
+        TextView driverCarModel = (TextView)findViewById(R.id.driverCarModelView);
+        TextView driverCarYear = (TextView)findViewById(R.id.driverCarYearView);
+        TextView driverCarSeats = (TextView)findViewById(R.id.driverCarYearView);
+        TextView driverCarLicense = (TextView)findViewById(R.id.driverCarLicensePlateView);
+        try {
+            driverName.setText(jsonDriver.getString("operator"));
+            driverRating.setText(jsonDriver.getString("rating"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            driverCarMake.setText(jsonCar.getString("make"));
+            driverCarModel.setText(jsonCar.getString("model"));
+            driverCarYear.setText(jsonCar.getString("year"));
+            driverCarSeats.setText(jsonCar.getString("numOfSeats"));
+            driverCarLicense.setText(jsonCar.getString("licensePlate"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 //      http request to get info
     }
 
@@ -48,7 +77,7 @@ public class DriverProfile extends Activity implements HttpRequestClient {
         // Then await until thread is finished to make the request synchronous.
         Thread t = new Thread(new Runnable() {
             // URL to target
-            final String tripUrl = URL.baseUrl + "getItineraryByUsername";
+            final String driverUrl = URL.baseUrl + URL.driverUrl + "getDriverByUsername";
 
             @Override
             public void run() {
@@ -56,11 +85,56 @@ public class DriverProfile extends Activity implements HttpRequestClient {
                 // Uses extended timeout
                 SyncHttpClient client = new SyncHttpClient();
                 client.setTimeout(HTTP.maxTimeoutExtended);
-                client.get(tripUrl, params, new AsyncHttpResponseHandler() {
+                client.get(driverUrl, params, new AsyncHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                         try {
-                            json = new JSONArray(new String(responseBody));
+                            jsonDriver = new JSONObject(new String(responseBody));
+                        } catch (JSONException e) {
+                            Log.e(TAG.driverProfileTag, "Failed to parse JSON from HTTP response");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers,
+                                          byte[] responseBody, Throwable error) {
+                        Log.e(TAG.driverProfileTag, "Server error - could not contact server");
+                    }
+                });
+            }
+        });
+        t.start();
+
+        // Await until thread t has finished its execution before proceeding
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            Log.e(TAG.driverProfileTag, "Thread exception in login thread");
+        }
+
+    }
+
+    public void syncHttpRequestCar() {
+        final RequestParams params = new RequestParams();
+        params.add("operator", username);
+
+        // Instantiate a new Runnable object which will handle http requests asynchronously
+        // Then await until thread is finished to make the request synchronous.
+        Thread t = new Thread(new Runnable() {
+            // URL to target
+            final String carUrl = URL.baseUrl + URL.carUrl + "getCar";
+
+            @Override
+            public void run() {
+                // Instantiate new synchronous http client, set timeout and perform get request
+                // Uses extended timeout
+                SyncHttpClient client = new SyncHttpClient();
+                client.setTimeout(HTTP.maxTimeoutExtended);
+                client.get(carUrl, params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        try {
+                            jsonCar = new JSONObject(new String(responseBody));
                         } catch (JSONException e) {
                             Log.e(TAG.driverProfileTag, "Failed to parse JSON from HTTP response");
                         }
